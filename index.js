926d04fcbabcb5f4367fd7c4696bced20de50728
rwandantechy <niyinnocent2027@.com>
2022-10-08 22:53:29 +0530

    new changes about the logic

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 const hello = () => {
-    
-    var name1= "cent";
-    console.log(" this is  written by " + "Inno"+name1 +"  at " + Date());
-   
-  };
-  
-hello();
-console.log(" Not only Genius are meant to code");
+  console.log('Hello Node.js!')
+}
+hello()
